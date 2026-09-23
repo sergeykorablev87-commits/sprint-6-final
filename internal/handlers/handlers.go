68a9a47d7067a82568b6dd265d64c0168b54a0f0
Sergey Korablev <sergey.korablev.87@mail.ru>
2026-09-23 22:22:26 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,74 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("index.html")
+	if err != nil {
+		http.Error(w, "Файл не найден", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, "Ошибка чтения файла", http.StatusInternalServerError)
+		return
+	}
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		http.Error(w, "Ошибка парсинга формы: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	file, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Ошибка получения файла: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Ошибка чтения файла: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	inputString := string(data)
+
+	result, err := service.ConvertAuto(inputString)
+	if err != nil {
+		http.Error(w, "Ошибка конвертации: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ext := filepath.Ext(header.Filename)
+	fileName := time.Now().UTC().String() + ext
+
+	newFile, err := os.Create(fileName)
+	if err != nil {
+		http.Error(w, "Ошибка создания файла: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = newFile.Write([]byte(result))
+	if err != nil {
+		http.Error(w, "Ошибка записи в файл: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(result))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,28 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func ConvertAuto(input string) (string, error) {
+	if strings.TrimSpace(input) == "" {
+		return "", errors.New("empty input")
+	}
+
+	isPureMorse := true
+
+	for _, char := range input {
+		if char != '.' && char != '-' && char != ' ' && char != '\n' && char != '\t' {
+			isPureMorse = false
+			break
+		}
+	}
+
+	if isPureMorse {
+		return morse.ToText(input), nil
+	}
+	return morse.ToMorse(input), nil
+}
